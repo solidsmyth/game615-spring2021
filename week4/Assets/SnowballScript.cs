94d62324238ea936fbfd550965919060abe6297b
solidsmyth <74252115+solidsmyth@users.noreply.github.com>
2021-02-23 20:49:41 -0500

    changes

---
+++ week4/Assets/SnowballScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnowballScript : MonoBehaviour
+{
+    public float speed = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = transform.position + transform.forward * speed * Time.deltaTime; 
+    }
+}
